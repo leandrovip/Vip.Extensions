83b1ab312ba63f8bb27788a27ca591e7f787473c
Leandro Ferreira <leandro@vipsolucoes.com>
2021-03-18 16:10:16 -0300

    Adiciona método NotNegative para int e decimal

--- src/Vip.Extensions/Types/Integer.cs
+++ src/Vip.Extensions/Types/Integer.cs
@@ -4,4 +4,9 @@
     {
         return value <= 0 ? 1 : value;
     }
+
+    public static int NotNegative(this int value, int defaultValue = 0)
+    {
+        return value < 0 ? defaultValue : value;
+    }
 }
---
+++ tests/Vip.Extensions.Tests/Helpers/TestNotNullOrEmptyString.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Vip.Extensions.Tests.Helpers
+{
+    public class TestNotNullOrEmptyString : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _data = new List<object[]>
+        {
+            new object[] {null},
+            new object[] {""}
+        };
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
