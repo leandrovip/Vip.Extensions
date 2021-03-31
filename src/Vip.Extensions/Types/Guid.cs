871aff2003d430589421d328996f6eb0d8d48f18
Leandro Ferreira <leandro@vipsolucoes.com>
2021-03-31 08:45:47 -0300

    Adiciona método IsEmpty para guid

---
+++ src/Vip.Extensions/Types/Guid.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static partial class Methods
+{
+    public static bool IsEmpty(this Guid value)
+    {
+        return value.Equals(Guid.Empty);
+    }
+
+    public static bool IsNotEmpty(this Guid value)
+    {
+        return !value.IsEmpty();
+    }
+}
---
+++ tests/Vip.Extensions.Tests/GuidTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace Vip.Extensions.Tests
+{
+    public class GuidTests
+    {
+        [Fact]
+        public void GuidTests_IsEmpty_DeveRetornarTrueCasoGuidForVazio()
+        {
+            // Arrange
+            var guidEmpty = Guid.Empty;
+
+            // Assert
+            Assert.True(guidEmpty.IsEmpty());
+        }
+
+        [Fact]
+        public void GuidTests_IsNotEmpty_DeveRetornarTrueCasoGuidNaoForVazio()
+        {
+            // Arrange
+            var guidEmpty = Guid.NewGuid();
+
+            // Assert
+            Assert.True(guidEmpty.IsNotEmpty());
+        }
+    }
+}
